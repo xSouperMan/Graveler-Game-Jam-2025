e19c0a7940b9e070c5678f5cef56fc485940d18f
Philip Mac <philip@Philips-MacBook-Pro.local>
2025-11-14 18:25:01 +0100

    changes

--- game-jam-2025/scripts/Player.cs
+++ game-jam-2025/scripts/Player.cs
@@ -11,8 +11,40 @@ public partial class Player : CharacterBody2D
         GlobalPosition = GlobalPosition.Snapped(Vector2.One * TILE_SIZE);
     }
 
+	private void HandleInput()
+    {
+        if(Input.IsActionPressed("move_up") && Input.IsActionPressed("move_down") ||
+		   Input.IsActionPressed("move_left") && Input.IsActionJustPressed("move_right")) {
+			return;
+		   }
+		if(Input.IsActionPressed("move_up"))
+        {
+            
+        } else if(Input.IsActionPressed("move_right"))
+        {
+            
+        } else if(Input.IsActionPressed("move_down"))
+        {
+            
+        } else if(Input.IsActionPressed("move_left"))
+        {
+            
+        }  
+    }
+
 	public override void _PhysicsProcess(double delta)
 	{
-		
+		if(isMoving)
+        {
+            MoveStep(delta);
+        } else
+        {
+            HandleInput();
+        }
 	}
+
+    private void MoveStep(double delta)
+    {
+        
+    }
 }
