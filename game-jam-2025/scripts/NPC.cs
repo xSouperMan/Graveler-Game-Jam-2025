794b6c44a84b384bdf63c9f31742e6016a69b351
Philip Mac <philip@Philips-MacBook-Pro.local>
2025-11-15 14:09:24 +0100

    small stuff

--- game-jam-2025/scripts/NPC.cs
+++ game-jam-2025/scripts/NPC.cs
@@ -2,10 +2,10 @@ using Godot;
 
 public partial class NPC : CharacterBody2D
 {
-    public NPCData Data { get; private set; }
+	public NPCData Data { get; private set; }
 
-    public void Initialize(NPCData data)
-    {
-        Data = data;
-    }
+	public void Initialize(NPCData data)
+	{
+		Data = data;
+	}
 }
