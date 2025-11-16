da2317f699883dc1ef724a0a42ab7efcf37a922a
Philip <philip@Philips-MacBook-Pro.local>
2025-11-16 12:34:24 +0100

    startmenu

--- enemies-of-the-state/scripts/StartMenu.cs
+++ enemies-of-the-state/scripts/StartMenu.cs
@@ -8,32 +8,32 @@ public partial class StartMenu : Control
 	[Export] Button lore;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-    {
-        start.Pressed += startPressed;
+	{
+		start.Pressed += startPressed;
 		controls.Pressed += controlsPressed;
 		lore.Pressed += lorePressed;
-    }
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-    {
-        if(Input.IsActionJustPressed("quit"))
-        {
-            GetTree().Quit();
-        }
-    }
+	{
+		if(Input.IsActionJustPressed("quit"))
+		{
+			GetTree().Quit();
+		}
+	}
 	public void startPressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/Map.tscn");
-    }
+	{
+		GetTree().ChangeSceneToFile("res://scenes/Map.tscn");
+	}
 
 	public void controlsPressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/Start_Controls.tscn");
-    }
+	{
+		GetTree().ChangeSceneToFile("res://scenes/Start_Controls.tscn");
+	}
 
 	public void lorePressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/Start_Lore.tscn");
-    }
+	{
+		GetTree().ChangeSceneToFile("res://scenes/Start_Lore.tscn");
+	}
 }
