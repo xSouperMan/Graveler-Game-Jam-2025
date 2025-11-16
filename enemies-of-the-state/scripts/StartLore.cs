d27593a92dd574bc3920b96b5aa0ef408bc88f07
Philip <philip@Philips-MacBook-Pro.local>
2025-11-16 12:37:20 +0100

    added scripts

---
+++ enemies-of-the-state/scripts/StartLore.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class StartLore : Control
+{
+	[Export] Button back;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		back.Pressed += backPressed;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(Input.IsActionJustPressed("quit"))
+		{
+			GetTree().Quit();
+		}
+	}
+	public void backPressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/Start_Menu.tscn");
+	}
+}
